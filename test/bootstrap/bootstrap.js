6d8b53f9d016255b454ad3012473afc4af4d4970
mohayonao <mohayonao@gmail.com>
2014-08-30 13:55:31 +0900

    remove unsed dependencies

--- test/bootstrap/bootstrap.js
+++ test/bootstrap/bootstrap.js
@@ -1,8 +1,5 @@
 "use strict";
 
 global.chai   = require("chai");
-global.sinon  = require("sinon");
 global.expect = global.chai.expect;
 global.window = global;
-
-global.chai.use(require("sinon-chai"));
