3d15e6bd4a97910fc750a1e5ac64e4c54e5be9f6
mohayonao <mohayonao@gmail.com>
2015-01-30 21:07:42 +0900

    update WebAudioTestAPI

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ if (global.WEB_AUDIO_TEST_API_IGNORE) {
   return;
 }
 
+var WebAudioAPI = {};
+
+WebAudioAPI.AudioContext = global.AudioContext || global.webkitAudioContext;
+WebAudioAPI.OfflineAudioContext = global.OfflineAudioContext || global.webkitOfflineAudioContext;
+
 var WebAudioTestAPI = {};
 
 WebAudioTestAPI.VERSION = "0.1.16";
@@ -18,5 +23,15 @@ global.WebAudioTestAPI = WebAudioTestAPI;
 
 global.AudioContext = require("./AudioContext");
 global.OfflineAudioContext = require("./OfflineAudioContext");
+
+WebAudioTestAPI.use = function() {
+  global.AudioContext = WebAudioTestAPI.AudioContext;
+  global.OfflineAudioContext = WebAudioTestAPI.OfflineAudioContext;
+};
+
+WebAudioTestAPI.unuse = function() {
+  global.AudioContext = WebAudioAPI.AudioContext;
+  global.OfflineAudioContext = WebAudioAPI.OfflineAudioContext;
+};
 
 module.exports = WebAudioTestAPI;
---
+++ test/WebAudioTestAPI.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var WebAudioTestAPI = require("../src");
+
+describe("WebAudioTestAPI", function() {
+
+  after(function() {
+    WebAudioTestAPI.use();
+  });
+
+  describe("VERSION", function() {
+    it("get: string", function() {
+      assert(typeof WebAudioTestAPI.VERSION === "string");
+      if (typeof global.WEB_AUDIO_TEST_API_VERSION === "string") {
+        assert(WebAudioTestAPI.VERSION === global.WEB_AUDIO_TEST_API_VERSION);
+      }
+    });
+  });
+
+  describe("sampleRate", function() {
+    it("get/set: number", function() {
+      assert(typeof WebAudioTestAPI.sampleRate === "number");
+    });
+  });
+
+  describe("unuse", function() {
+    it("(): void", function() {
+      WebAudioTestAPI.unuse();
+
+      assert(global.AudioContext !== WebAudioTestAPI.AudioContext);
+      assert(global.OfflineAudioContext !== WebAudioTestAPI.OfflineAudioContext);
+    });
+  });
+
+  describe("use", function() {
+    it("(): void", function() {
+      WebAudioTestAPI.use();
+
+      assert(global.AudioContext === WebAudioTestAPI.AudioContext);
+      assert(global.OfflineAudioContext === WebAudioTestAPI.OfflineAudioContext);
+    });
+  });
+});
