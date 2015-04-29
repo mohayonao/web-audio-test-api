784171baedbf0c62f9c917defd5450265fa0f179
mohayonao <mohayonao@gmail.com>
2015-04-29 10:16:52 +0900

    update dev tools

--- test/bootstrap/bootstrap.js
+++ test/bootstrap/bootstrap.js
@@ -10,11 +10,6 @@ global.closeTo = function(actual, expected, delta) {
 
 global.WEB_AUDIO_TEST_API_VERSION = require("../../package").version;
 
-require("espower-loader")({
-  cwd: process.cwd(),
-  pattern: "test/**/*.js"
-});
-
-global._  = require("underscore");
+global._  = require("lodash");
 
 require("../../");
